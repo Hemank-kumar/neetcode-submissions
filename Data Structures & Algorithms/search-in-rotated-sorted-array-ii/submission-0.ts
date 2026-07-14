d76d0dfe94ebd1d68ba4482cfb30f4566b1abef4
Hemank Kumar <hemank.fsu@gmail.com>
2026-07-14 19:14:47 +0530

    Add: search-in-rotated-sorted-array-ii - submission-0

---
+++ Data Structures & Algorithms/search-in-rotated-sorted-array-ii/submission-0.ts
@@ -0,0 +1,10 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {boolean}
+     */
+    search(nums: number[], target: number): boolean {
+        return (nums.indexOf(target) >=0);
+    }
+}
