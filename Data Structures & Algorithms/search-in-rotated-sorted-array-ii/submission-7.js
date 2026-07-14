af608e737f28c25b63d62795ff343f897d2ddc64
Hemank Kumar <hemank.fsu@gmail.com>
2026-07-14 19:46:45 +0530

    Add: search-in-rotated-sorted-array-ii - submission-7

---
+++ Data Structures & Algorithms/search-in-rotated-sorted-array-ii/submission-7.js
@@ -0,0 +1,10 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {boolean}
+     */
+    search(nums, target) {
+        return (nums.indexOf(target) >= 0);
+    }
+}
