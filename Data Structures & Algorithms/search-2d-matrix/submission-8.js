21872c69190bc6d5b6c00ca1334025eab1667b3a
Hemank Kumar <hemank.fsu@gmail.com>
2026-07-17 01:42:20 +0530

    Add: search-2d-matrix - submission-8

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-8.js
@@ -0,0 +1,30 @@
+class Solution {
+    /**
+     * @param {number[][]} matrix
+     * @param {number} target
+     * @return {boolean}
+     */
+    searchMatrix(matrix, target) {
+        const n = matrix.length;
+        const m = matrix[0].length;
+
+        let left = 0;
+        let right = m * n - 1;
+
+        while(left <= right){
+
+            const mid = Math.floor(left + (right - left) / 2);
+            const midVal = matrix[Math.floor(mid / m)][Math.floor(mid % m)];
+
+            if(midVal === target){
+                return true;
+            }
+            if(midVal < target){
+                left = mid + 1;
+            }else{
+                right = mid - 1;
+            }
+        }
+        return false;
+    }
+}
