612308ada25e1d5beb0a5ce989bf111f16f85c88
Hemank Kumar <hemank.fsu@gmail.com>
2026-07-24 05:29:47 +0530

    Add: find-first-and-last-position-of-element-in-sorted-array - submission-10

---
+++ Data Structures & Algorithms/find-first-and-last-position-of-element-in-sorted-array/submission-10.ts
@@ -0,0 +1,48 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number[]}
+     */
+    searchRange(nums: number[], target: number): number[] {
+        if(nums == null || nums.length == 0)return [-1,-1];
+        let first = this.firstIndex(nums,target);
+        let last = this.lastIndex(nums,target);
+        return [first,last];
+    }
+    firstIndex(nums:number[],target:number){
+        let left = 0;
+        let right = nums.length - 1;
+        let ans = -1;
+        while(left <= right){
+            let mid = left + Math.floor((right - left)/2);
+            if(nums[mid] == target){
+                ans = mid;
+                right = mid - 1;
+            }else if(nums[mid] < target){
+                left = mid + 1;
+            }else{
+                right = mid -1;
+            }
+        }
+        return ans;
+    }
+    lastIndex(nums:number[],target:number){
+        let left = 0;
+        let right = nums.length - 1;
+        let ans = -1;
+        while(left <= right){
+            let mid = left + Math.floor((right - left)/2);
+            if(nums[mid] == target){
+                ans = mid;
+                left = mid + 1;
+            }else if(nums[mid] < target){
+                left = mid + 1;
+            }else{
+                right = mid -1;
+            }
+        }
+        return ans;
+    }
+    
+}
